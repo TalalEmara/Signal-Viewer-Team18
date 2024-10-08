05b42db8697ca224b83cc12bcac3b0ba2cfd8e04
TalalEmara <talalmemara@gmail.com>
2024-10-08 21:23:01 +0300

    Signal Controls Buttons| Adding Icons

--- GUI/Styles.py
+++ GUI/Styles.py
@@ -8,10 +8,37 @@ toolBarStyle = """
 QWidget{
     background-color: #2D2D2D;
         color: #76D4D4;
+    
 }
 """
 boxStyle = """
-QHBoxLayout{
+QWidget{
     background-color: #2D2D2D;
+    color: #76D4D4;
+    border-radius: 10px;   
+    padding: 4px
+}
+"""
+signalControlButtonStyle = """
+QPushButton{
+    background-color: #2d2d2d;
+    border: 1px solid #76D4D4;
+    border-radius: 5px;
+}
+"""
+rewindButtonStyle = """
+QPushButton{
+    background-color: #76D4D4;
+    border: 1px solid #76D4D4;
+    border-radius: 5px;
+    
+}
+"""
+timeLabelStyle = """
+QLabel {
+    color: #76D4D4;
+    font-family: Sofia sans;
+    font-weight: semiBold;
+    font-size: 20px;
 }
 """
--- GUI/tool_bar.py
+++ GUI/tool_bar.py
@@ -1,4 +1,8 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QPushButton, QLabel, QHBoxLayout, QVBoxLayout, QCheckBox, QWidget
+from PyQt5.QtGui import QIcon
+from Styles import boxStyle, signalControlButtonStyle, timeLabelStyle, rewindButtonStyle
+
 
 class ToolBar(QWidget):
     def __init__(self):
@@ -6,25 +10,57 @@ class ToolBar(QWidget):
 
         # Signal controls and time
         self.timeLabel = QLabel("00:00")
-        self.pauseButton = QPushButton("||")
-        self.startButton = QPushButton(">")
-        self.stopButton = QPushButton("0")
-        self.toStartButton = QPushButton("|<")
-        self.toEndButton = QPushButton(">|")
+        self.timeLabel.setAlignment(Qt.AlignCenter)
+        self.timeLabel.setStyleSheet(timeLabelStyle)
+
+        self.pauseButton = QPushButton()
+        self.pauseButton.setStyleSheet(signalControlButtonStyle)
+        self.pauseIcon = QIcon("Assets/ControlsButtons/pause.png")
+        self.pauseButton.setIcon(self.pauseIcon)
+        self.pauseButton.resize(25, 25)
+
+        self.playButton = QPushButton()
+        self.playButton.setStyleSheet(signalControlButtonStyle)
+        self.playIcon = QIcon("Assets/ControlsButtons/play.png")
+        self.playButton.setIcon(self.playIcon)
+        self.playButton.resize(25, 25)
+
+        self.stopButton = QPushButton()
+        self.stopButton.setStyleSheet(signalControlButtonStyle)
+        self.stopIcon = QIcon("Assets/ControlsButtons/stop.png")
+        self.stopButton.setIcon(self.stopIcon)
+        self.stopButton.resize(25, 25)
+
+        self.toStartButton = QPushButton()
+        self.toStartButton.setStyleSheet(signalControlButtonStyle)
+        self.toStartIcon = QIcon("Assets/ControlsButtons/start.png")
+        self.toStartButton.setIcon(self.toStartIcon)
+        self.toStartButton.resize(25, 25)
+
+        self.toEndButton = QPushButton()
+        self.toEndButton.setStyleSheet(signalControlButtonStyle)
+        self.toEndIcon = QIcon("Assets/ControlsButtons/end.png")
+        self.toEndButton.setIcon(self.pauseIcon)
+        self.toEndButton.resize(25, 25)
+
+        self.rewindButton = QPushButton()
+        self.rewindButton.setStyleSheet(rewindButtonStyle)
+        self.rewindIcon = QIcon("Assets/ControlsButtons/rewindOff.png")
+        self.rewindButton.setIcon(self.rewindIcon)
+        self.rewindButton.resize(25, 25)
 
         signalControl = QWidget()
+        signalControl.setStyleSheet(boxStyle)
         signalControlLayout = QHBoxLayout()
         signalControl.setLayout(signalControlLayout)
 
         signalControlLayout.addWidget(self.timeLabel)
         signalControlLayout.addWidget(self.pauseButton)
-        signalControlLayout.addWidget(self.pauseButton)
-        signalControlLayout.addWidget(self.startButton)
+        signalControlLayout.addWidget(self.playButton)
         signalControlLayout.addWidget(self.stopButton)
         signalControlLayout.addWidget(self.toStartButton)
         signalControlLayout.addWidget(self.toEndButton)
-
-
+        signalControlLayout.addWidget(self.rewindButton)
 
         self.zoomButton = QPushButton("Z")
         self.glueButton = QPushButton("G")
@@ -33,7 +69,7 @@ class ToolBar(QWidget):
         toolGroup.addWidget(self.zoomButton)
         toolGroup.addWidget(self.glueButton)
 
-        self.linkedLabel =QLabel("Linked")
+        self.linkedLabel = QLabel("Linked")
         self.channel1LinkOption = QPushButton("Channel 1")
         self.channel2LinkOption = QPushButton("Channel 2")
 
