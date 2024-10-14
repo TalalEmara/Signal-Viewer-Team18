edd681e4498dd0bf97a16080346fbd92cb6306b3
Nouran Hani <nouran5hani@gmail.com>
2024-10-14 18:11:14 +0300

    live signal update

--- Core/live_signal.py
+++ Core/live_signal.py
@@ -11,22 +11,9 @@ url = 'https://services.swpc.noaa.gov/json/planetary_k_index_1m.json'
 
 
 # Function to fetch and process the live solar wind data
-def fetch_solar_wind_data():
+def live_signal(url):
     response = requests.get(url)
     data = response.json()
-
-    # # Extract time and solar wind speed from the data
-    # times = [
-    #     ((str(datetime.strptime(entry[0], '%Y-%m-%d %H:%M:%S.%f') - 
-    #     datetime.strptime(entry[0][:10], '%Y-%m-%d')))) 
-    #     for entry in data[1:]
-    # ]
-
-    # # Extract speed and handle None values by skipping them or replacing with 0
-    # speed = [
-    #     float(entry[2]) if entry[2] is not None else 0  # Set speed to 0 if it's None
-    #     for entry in data[1:]
-    # ]
 
     # Extracting time in hour:min:sec format and saving the date
     time = []
