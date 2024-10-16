075ce6f2af250849ab6117c237138b70be482ad5
TalalEmara <talalmemara@gmail.com>
2024-10-16 13:35:57 +0300

    Create SignalClass.py

---
+++ Core/SignalClass.py
@@ -0,0 +1,11 @@
+
+class Signal:
+    def __init__(self):
+        self.name = "Untitled"
+        self.location = "////"
+        self.color = "#EFEFEF"
+        self.lineWidth = 20
+        self.dataSet = []
+        self.isLive = False
+        self.isOnChannel1 = True
+        self.isOnChannel2 = False
