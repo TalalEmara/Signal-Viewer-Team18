5d1e05507c8895593b3db679678f493391d9726e
TalalEmara <talalmemara@gmail.com>
2024-10-16 13:37:24 +0300

    Update SignalClass.py

--- Core/SignalClass.py
+++ Core/SignalClass.py
@@ -3,9 +3,13 @@ class Signal:
     def __init__(self):
         self.name = "Untitled"
         self.location = "////"
-        self.color = "#EFEFEF"
         self.lineWidth = 20
         self.dataSet = []
         self.isLive = False
         self.isOnChannel1 = True
         self.isOnChannel2 = False
+
+        self.colorinChannel1 = "#EFEFEF"
+        self.colorinChannel2 = "#EFEFEF"
+        self.isShown =True
+
